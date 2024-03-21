0ec6da443aaf8eecd08c6ef1fb791c7e2c0df21f
M3nt4lG3n <143116925+M3nt4lG3n@users.noreply.github.com>
2024-03-21 13:21:41 -0400

    First Commit

---
+++ Oregon-Trail/src/Oregon_Trail_Package/Oregon_Trail_Window.java
@@ -0,0 +1,43 @@
+package Oregon_Trail_Package;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Oregon_Trail_Window {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Oregon_Trail_Window window = new Oregon_Trail_Window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Oregon_Trail_Window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
