d7285b313b275e96e9a60d0cf95d6890d1b7bb1e
M3nt4lG3n <143116925+M3nt4lG3n@users.noreply.github.com>
2024-03-24 23:25:04 -0400

    Arraylist is being filled, methods not completed to access the data yet

--- Oregon-Trail/src/Oregon_Trail_Package/ItemClass.java
+++ Oregon-Trail/src/Oregon_Trail_Package/ItemClass.java
@@ -1,51 +1,74 @@
 package Oregon_Trail_Package;
 
+import java.awt.EventQueue;
+import java.awt.ItemSelectable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ItemClass {
-	String itemName = "";
-	String itemType = "";
-	int itemWeight = 0;
-	boolean itemSelected = false;
-	boolean itemSubstantial = false;
-	
-	ItemClass(String name, String type, int weight, boolean selected, boolean substantial) {
+	static ArrayList<ItemClass> items = new ArrayList<ItemClass>();
 		
+	public ItemClass(String name, String type, int weight, boolean selected, boolean substantial) {
+		String itemName = name;
+		String itemType = type;
+		int itemWeight = weight;
+		boolean itemSelected = selected;
+		boolean itemSubstantial = substantial;
+	}
+	
+	public void setStats() {
+		//Opening the CSV File and setting up the input scanner
+		InputStreamReader in = new InputStreamReader(this.getClass().getResourceAsStream("/Oregon_Trail_Package/WagonItems.csv"));
+		Scanner scr = new Scanner(in);
+		//Loop through all of the items in the csv file
+		//Get each value without the commas and put them into a object variable
+		//set that full object into an arrayList
+		//CSV Order: String Name, String Type, Int Weight, Bool Selected, Bool Substantial
+		while(scr.hasNext()) {
+			Scanner reader = new Scanner(scr.nextLine());
+			reader.useDelimiter(",");
+			String name = reader.next();
+			String type = reader.next();
+			String tempWeight = reader.next();
+			int weight = Integer.parseInt(tempWeight);
+			String tempSelected = reader.next();
+			boolean selected = Boolean.parseBoolean(tempSelected);
+			String tempSubstantial = reader.next();
+			boolean substantial = Boolean.parseBoolean(tempSubstantial);
+			ItemClass object = new ItemClass(name, type, weight, selected, substantial);
+			setArrayList(object);
+		}
+	}
+	
+	public void setArrayList(ItemClass item) {
+		items.add(item);
 	}
 
-	
-	public void setName(String name) {
-		
-	}
-
-	public String getName(String name) {
-		return name;
-	}
-	
-	public void setType(String type) {
-		
-	}
+	//public String getName(int index) {
+		//Iterator<ItemClass> object = items.iterator();
+		//while(object.hasNext()) {
+			//ItemClass ic = object.next();
+			//String name = ic.itemNameGet();
+		//}
+		//return name;
+	//}
 	
 	public String getType(String type) {
 		return type;
-	}
-
-	public void setWeight(int weight) {
-		
 	}
 	
 	public int getWeight(int weight) {
 		return weight;
 	}
 	
-	public void setSelected(boolean selected) {
-		
-	}
-	
 	public boolean getSelected(boolean selected) {
 		return selected;
-	}
-	
-	public void setSubstantial(boolean substantial){
-		
 	}
 	
 	public boolean getSubstantial(boolean substantial) {
--- Oregon-Trail/src/Oregon_Trail_Package/Oregon_Trail_Window.java
+++ Oregon-Trail/src/Oregon_Trail_Package/Oregon_Trail_Window.java
@@ -5,6 +5,7 @@ import java.awt.ItemSelectable;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -44,36 +45,14 @@ public class Oregon_Trail_Window {
 	 * Initialize the contents of the frame.
 	 * @throws FileNotFoundException 
 	 */
-	private void initialize() throws FileNotFoundException {
-		ArrayList<ItemClass> items = new ArrayList<ItemClass>();
+	private void initialize(){
+		ItemClass itemClass = new ItemClass(null, null, 0, false, false);
+		itemClass.setStats();
 		
 		frame = new JFrame();
 		frame.setBounds(200, 200, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		//Opening the CSV File and setting up the input scanner
-		File inputFile = new File("WagonItems.csv");
-		Scanner in = new Scanner(inputFile);
-		in.useDelimiter(",");
-		//Loop through all of the items in the csv file
-		//Get each value without the commas and put them into a object variable
-		//set that full object into an arrayList
-		//CSV Order: String Name, String Type, Int Weight, Bool Selected, Bool Substantial
-		while(in.hasNext()) {
-			String name = in.next();
-			String type = in.next();
-			String tempWeight = in.next();
-			int weight = Integer.parseInt(tempWeight);
-			String tempSelected = in.next();
-			boolean selected = Boolean.parseBoolean(tempSelected);
-			String tempSubstantial = in.next();
-			boolean substantial = Boolean.parseBoolean(tempSubstantial);
-			
-			
-			ItemClass item = new ItemClass(name, type, weight, selected, substantial);
-			items.add(item);
-		}
 	}
 
 }
